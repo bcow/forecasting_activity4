d2b08231645be67fbb45aac78312c67e694ddbb7
Alexey Shiklomanov (LVM64) <alexey.shiklomanov@gmail.com>
2016-02-17 11:35:27 -0500

    Add basic prairie phenology workflow

---
+++ exercise.R
@@ -0,0 +1,16 @@
+#' ---
+#' title: "Forecasting: Exercise 4"
+#' author: Alexey Shiklomanov, ???
+#' ---
+
+### Prairie Phenology Workflow
+
+## Download phenology data
+
+## Plot overall phenology data
+
+## Create and visualize subset of data for leaf out
+
+## Fit logistic model
+
+## Visualize model and data
